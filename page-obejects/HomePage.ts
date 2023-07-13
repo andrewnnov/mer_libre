ad7c9354fc58b3272171824675d2592a81c7c99e
andrewnnov <andrewnnov@yandex.ru>
2023-07-13 18:24:30 -0300

    create home page

--- page-obejects/HomePage.ts
+++ page-obejects/HomePage.ts
@@ -2,11 +2,19 @@ import { expect, Locator, Page } from '@playwright/test'
 
 export class HomePage {
   readonly page: Page
+  readonly categoriasLink: Locator
+  readonly ofertasLink: Locator
+  readonly historialLink: Locator
+  readonly superMercadoLink: Locator  
   readonly masTardeButton: Locator
 
   constructor(page: Page) {
     this.page = page
     this.masTardeButton = page.locator("//div[@class='andes-tooltip__buttons']//span[contains(text(), 'Más tarde')]")
+    this.categoriasLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Categorías')]")
+    this.ofertasLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Ofertas')]")
+    this.historialLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Historial')]")
+    this.superMercadoLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Supermercado')]")
   }
 
   async visit() {
@@ -17,6 +25,11 @@ export class HomePage {
   clickMasTardeButton = async () => {
     await this.masTardeButton.click();
     await this.page.pause();
+  }
+
+  async clickCategory(name: String) {
+    
+
   }
 
 }
