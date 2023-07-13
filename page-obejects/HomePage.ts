02d2b5bada08f1e6fa126b1e53a885c83769973c
andrewnnov <andrewnnov@yandex.ru>
2023-07-13 18:43:20 -0300

    first tests

--- page-obejects/HomePage.ts
+++ page-obejects/HomePage.ts
@@ -5,31 +5,48 @@ export class HomePage {
   readonly categoriasLink: Locator
   readonly ofertasLink: Locator
   readonly historialLink: Locator
-  readonly superMercadoLink: Locator  
+  readonly superMercadoLink: Locator
   readonly masTardeButton: Locator
 
   constructor(page: Page) {
     this.page = page
-    this.masTardeButton = page.locator("//div[@class='andes-tooltip__buttons']//span[contains(text(), 'Más tarde')]")
-    this.categoriasLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Categorías')]")
-    this.ofertasLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Ofertas')]")
-    this.historialLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Historial')]")
-    this.superMercadoLink = page.locator("//li[@class='nav-menu-item']//a[contains(text(), 'Supermercado')]")
+    this.masTardeButton = page.locator(
+      "//div[@class='andes-tooltip__buttons']//span[contains(text(), 'Más tarde')]",
+    )
+    this.categoriasLink = page.locator(
+      "//li[@class='nav-menu-item']//a[contains(text(), 'Categorías')]",
+    )
+    this.ofertasLink = page.locator(
+      "//li[@class='nav-menu-item']//a[contains(text(), 'Ofertas')]",
+    )
+    this.historialLink = page.locator(
+      "//li[@class='nav-menu-item']//a[contains(text(), 'Historial')]",
+    )
+    this.superMercadoLink = page.locator(
+      "//li[@class='nav-menu-item']//a[contains(text(), 'Supermercado')]",
+    )
   }
 
   async visit() {
-    await this.page.goto("https://www.mercadolibre.com.ar/")
+    await this.page.goto('https://www.mercadolibre.com.ar/')
     //await this.page.waitForTimeout(10000);
   }
 
   clickMasTardeButton = async () => {
-    await this.masTardeButton.click();
-    await this.page.pause();
+    await this.masTardeButton.click()    
   }
 
-  async clickCategory(name: String) {
-    
-
+  async clickOfertaCategory() {
+    await this.ofertasLink.waitFor()
+    await this.ofertasLink.click()
+    await this.page.waitForURL(/\/ofertas/, { timeout: 3000 })
   }
+
+  async clickHistorialCategory() {
+    await this.historialLink.waitFor()
+    await this.historialLink.click()
+    await this.page.waitForURL(/\/home/, { timeout: 3000 })
+  }
+
 
 }
--- tests/test-example.spec.ts
+++ tests/test-example.spec.ts
@@ -1,15 +1,22 @@
 import { test } from '@playwright/test'
 import { HomePage } from '../page-obejects/HomePage'
 
-test.describe.only('Test', async () => {
+test.describe.parallel.only('Enable basic links', async () => {
   let homePage: HomePage
 
   test.beforeEach(async ({ page }) => {
     homePage = new HomePage(page)
     await homePage.visit()
+    await homePage.clickMasTardeButton()
   })
 
-  test("Click mas tarde", async ({page}) => {
-    await homePage.clickMasTardeButton();
-  })  
+  test('Choose offertas link', async ({ page }) => {
+    await homePage.clickOfertaCategory()
+  })
+
+  test('Choose historial link', async ({ page }) => {
+    await homePage.clickHistorialCategory()
+  })
+
+
 })
